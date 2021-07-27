8ec712c384436a4ed8e877713bc564203d18c262
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-07-28 00:43:57 +0700

    Array javascript

---
+++ 00. Javasript/Array.js
@@ -0,0 +1,25 @@
+var array = [1, 2, 3];
+console.log(array);
+
+// fungsi array
+
+// array push
+// fungsi push untuk menambah data kedalam array
+var array1 = [1, 2, 3];
+array1.push(4);
+// [ 1, 2, 3, 4 ]
+console.log(array1);
+
+// array pop
+// fungsi pop adalah untuk menghapus data index terakhir dari array
+var array2 = [1, 2, 3];
+array2.pop();
+// [ 1, 2 ]
+console.log(array2);
+
+// array shift
+// shift adalah kebalikan dari pop , shift menghapus data index pertama dari array
+var array3 = [1, 2, 3];
+array3.shift();
+// [ 2, 3 ]
+console.log(array3);
