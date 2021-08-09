f2a2a24484689d66de27113f117b8ed1e418cafe
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-08-09 22:28:02 +0700

    init project ?

---
+++ database/migrations/2021_08_09_110433_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('user_id')->nullable();
+            $table->integer('food_id')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->integer('total')->nullable();
+            $table->string('status')->nullable();
+            $table->text('payment_url')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
